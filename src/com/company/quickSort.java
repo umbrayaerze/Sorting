66c7ec36ae6816e1b5c20b60a03a7bd93b421b28
liene-ma <liene.malkalne@gmail.com>
2021-02-05 18:24:35 +0000

    n

---
+++ src/com/company/quickSort.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Random;
+
+public class quickSort {
+
+    public static void main(String[] args) {
+        //Random rd = new Random();
+        int[] arr = new int[20000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = getRandomNumberUsingNextInt(-20000, 20000);
+            System.out.println(arr[i]);
+        }
+    }
+    public static int getRandomNumberUsingNextInt(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
+
+
+}
+
+

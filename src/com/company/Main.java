12b3c197bcf612aebb1053eda2761ba300f6397c
R-Alisa <77399056+R-Alisa@users.noreply.github.com>
2021-02-05 20:05:06 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,83 @@
 package com.company;
 
-public class Main {
+import java.util.Random;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    public void sort(int arr[])
+    {
+        int n = arr.length;
+
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+
+        for (int i = n - 1; i > 0; i--) {
+
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+
+            heapify(arr, i, 0);
+        }
+    }
+
+
+    void heapify(int arr[], int n, int i)
+    {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+
+            heapify(arr, n, largest);
+        }
+    }
+
+
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+
+    public static void main(String args[]){
+
+ /* my modicfications start here
+    {
+        int arr[] = { 2,6,9};
+        int n = arr.length;
+        Main ob = new Main();
+        ob.sort(arr);
+        System.out.println("Sorted array is");
+        printArray(arr);
+    }
+    */
+       int[] arr = new int[20];
+       int n = arr.length;
+         arr[i] =getRandomNumberUsingNextInt(1,100);
+
+    public int getRandomNumberUsingNextInt(int min, int max){
+            Random random = new Random();
+            return random.nextInt(max - min) + min;
+        }
     }
 }

7916897e673155d66a0255500a72384f02fe093b
liene-ma <liene.malkalne@gmail.com>
2021-02-05 20:11:38 +0000

    changed class names and code for quickSort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,11 @@
 package com.company;
-import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        //Random rd = new Random();
-        int[] arr = new int[20];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = getRandomNumberUsingNextInt(1, 90);
-            System.out.println(arr[i]);
-        }
-    }
-    public static int getRandomNumberUsingNextInt(int min, int max) {
-        Random random = new Random();
-        return random.nextInt(max - min) + min;
-    }
-
-    public static
-
+        quickSort.sort(generateRandomNr.getArray(), 0, 20000);
 
 
     }
 
+}
--- src/com/company/quickSort.java
+++ src/com/company/quickSort.java
@@ -1,23 +1,68 @@
 package com.company;
 
-import java.util.Random;
-
 public class quickSort {
 
-    public static void main(String[] args) {
-        //Random rd = new Random();
-        int[] arr = new int[20000];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = getRandomNumberUsingNextInt(-20000, 20000);
-            System.out.println(arr[i]);
+    static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1); // index of smaller element
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than the pivot
+            if (arr[j] < pivot) {
+                i++;
+
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    static void sort(int arr[], int low, int high) {
+        if (low < high) {
+            /* pi is partitioning index, arr[pi] is
+              now at right place */
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before
+            // partition and after partition
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
         }
     }
-    public static int getRandomNumberUsingNextInt(int min, int max) {
-        Random random = new Random();
-        return random.nextInt(max - min) + min;
+
+
+
+    /* A utility function to print array of size n */
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
     }
 
+    // Driver program
+    public static void main(String args[]) {
+        int arr[] = {10, 7, 8, 9, 1, 5};
+        int n = arr.length;
 
+        com.company.quickSort ob = new com.company.quickSort();
+        ob.sort(arr, 0, n - 1);
+
+        System.out.println("sorted array");
+        printArray(arr);
+
+    }
 }
+
+
+
 
 

327a86af3a22fe9ccb40e9aea5b845d1160e6831
umbrayaerze <akhundovaee@gmail.com>
2021-02-08 19:39:02 +0200

    main class updated w/ other sorting algorithms

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,5 +19,16 @@ public class Main {
     public static void main(String[] args) {
         int[] myDirtyArray = randomArray(20000);
         System.out.println(Arrays.toString(myDirtyArray));
+
+        long startTime = System.currentTimeMillis();
+        MergeSort.mergeSort(myDirtyArray, 0, myDirtyArray.length-1);
+//        Heapsort;
+//        quickSort;
+        System.out.println(Arrays.toString(myDirtyArray));
+        long endTime = System.currentTimeMillis();
+        System.out.println("That took " + (endTime - startTime) + " milliseconds");
+
+
+
     }
 }

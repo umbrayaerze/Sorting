3ff801835991bd0a682c11d37eecb07bf781d4c5
liene-ma <liene.malkalne@gmail.com>
2021-02-10 18:41:53 +0000

    changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        quickSort.sort(generateRandomNr.getArray(), 0, 20000);
+
 
 
     }
--- src/com/company/quickSort.java
+++ src/com/company/quickSort.java
@@ -31,35 +31,13 @@ public class quickSort {
               now at right place */
             int pi = partition(arr, low, high);
 
-            // Recursively sort elements before
-            // partition and after partition
+            /*Recursively sort elements before
+            partition and after partition */
             sort(arr, low, pi - 1);
             sort(arr, pi + 1, high);
         }
     }
 
-
-
-    /* A utility function to print array of size n */
-    static void printArray(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-
-    // Driver program
-    public static void main(String args[]) {
-        int arr[] = {10, 7, 8, 9, 1, 5};
-        int n = arr.length;
-
-        com.company.quickSort ob = new com.company.quickSort();
-        ob.sort(arr, 0, n - 1);
-
-        System.out.println("sorted array");
-        printArray(arr);
-
-    }
 }
 
 
